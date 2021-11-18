228ee34243e6b8d41151f83e3a7527647fc9ac6c
cvalong <chris.va.long@gmail.com>
2021-11-18 11:48:04 -0800

    added random location and shop_type functions, then tested in the generator file

--- DND5eShopGenerator.py
+++ DND5eShopGenerator.py
@@ -33,3 +33,18 @@ Create a String Variable for shop_description
 Create a String Variable for shopkeeper
 Create an empty list or dictionary for items
 '''
+
+from magic_item_shop import magic_item_shop, random_location, random_shop_type
+
+#initialization
+magic_shop = magic_item_shop("Benny's Booming Bargains", "Metropolis", "Armorer")
+# magic_shop_location = magic_shop.location
+# magic_shop_type = magic_shop.shop_type
+# magic_shop_location_desc = magic_shop.location_description
+# magic_shop_shop_desc = magic_shop.shop_description
+# magic_shop_shopkeeper = magic_shop.shopkeeper
+# magic_shop_stock_list = magic_shop.item_stock
+
+# print(magic_shop.__dict__)
+print(random_location())
+print(random_shop_type())
--- magic_item_shop.py
+++ magic_item_shop.py
@@ -1,14 +1,31 @@
+from random import randint
+
 class magic_item_shop:
     def __init__(self, name, location, shop_type, 
-                location_description="", shopkeeper="",item_stock=[]):
+                location_description="", shop_description="", shopkeeper="",item_stock=[]):
         self.name = name
         self.location = location
         self.shop_type = shop_type
         self.location_description = location_description
+        self.shop_description = shop_description
         self.shopkeeper = shopkeeper
         self.item_stock = item_stock
 
-magic_shop = magic_item_shop("Benny's Booming Bargains", "Metropolis", "Armorer")
-print(magic_shop.name)
-print(magic_shop.location)
-print(magic_shop.shop_type)
+location_dict = {1: "Thorp", 2: "Hamlet", 3: "Village", 4: "Small Town", 5: "Large Town", 
+                6: "Small City", 7: "Large_City", 8: "Metropolis"}
+
+shop_type_dict = {1: "Trader", 2: "Armorer", 3: "Weaponsmith",
+                4: "Alchemist", 5: "Scribe", 6: "Wandwright"}
+
+def stock_reader():
+        pass
+
+def random_location():
+        return location_dict[randint(1,len(location_dict))]
+
+def random_shop_type():
+        return shop_type_dict[randint(1, len(shop_type_dict))]
+
+# magic_shop = magic_item_shop("Benny's Booming Bargains", "Metropolis", "Armorer")
+# print(random_location())
+
